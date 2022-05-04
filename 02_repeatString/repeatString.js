59b97329fc485a714d507da258e0ce727db304bf
Alex <18722853@student.westernsydney.edu.au>
2022-05-04 17:37:51 +1000

    completed exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
+const repeatString = function(inputString, inputNum) {
+    let concatString = "";
 
+    if(inputNum < 0) return 'ERROR';
+
+    for(i = 1; i <=inputNum; i++) {
+        concatString += inputString;
+    }
+
+    return concatString;
 };
-
+ 
 // Do not edit below this line
 module.exports = repeatString;
