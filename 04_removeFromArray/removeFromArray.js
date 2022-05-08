3e25dc8d560971877489bf73ad1dc61d23f8e6e5
Alex <18722853@student.westernsydney.edu.au>
2022-05-08 14:43:55 +1000

    completed removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(...manyArgs) {
+    //grab the main array
+    let initialArray = manyArgs[0];
+    
+    //initiate empty array to push correct values to
+    let finalArray = [];
 
-};
+    //arrow function for each item in initialArray, keep looping through
+    initialArray.forEach((item) => {
+        //if any other value passed after the array is present, don't push this value in initialArray to finalArray
+        if (!manyArgs.includes(item)) {
+            finalArray.push(item);
+        }
+      });
+
+    return finalArray;
+}; 
 
 // Do not edit below this line
 module.exports = removeFromArray;
