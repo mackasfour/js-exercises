bd70be06e81bb35a7b3dcbf5f683eafa39e20dc8
Alex <18722853@student.westernsydney.edu.au>
2022-05-08 15:10:57 +1000

    completed sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,26 @@
-const sumAll = function() {
+const sumAll = function(lowerValue, higherValue) {
+    //Ensure all passed values are positive integers (probably not the most efficient check)
+    if(!Number.isInteger(lowerValue) || !Number.isInteger(higherValue) || lowerValue < 0 || higherValue < 0) {
+        return("ERROR");
+    }
 
+    //check lowerValue and higherValue are appropriately assigned
+    if (lowerValue > higherValue) {
+        const holding = lowerValue;
+        lowerValue = higherValue;
+        higherValue = holding;
+    }
+
+    //initialise a variable to sum values too
+    let sum = 0;
+
+    for(let i = lowerValue; i <= higherValue; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+ 
