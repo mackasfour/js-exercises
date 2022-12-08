e5fbaccc8bdc2c68e518b5e189e457914f0c4c16
mackasfour <amclemon@gmail.com>
2022-12-09 10:27:42 +1100

    getTheTitles exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+    let bookNames = [];
+    for(i = 0;i < books.length;i++) {
+        bookNames.push(books[i].title);
+    }
 
+    return bookNames;
 };
 
 // Do not edit below this line
