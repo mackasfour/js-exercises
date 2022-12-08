5366d1a48807269f6eeaa691b96298ac6992482d
mackasfour <amclemon@gmail.com>
2022-12-08 18:40:52 +1100

    palindrome exercises

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
+const palindromes = function (input) {
+    modifiedString = input.toLowerCase().replace(/[^A-Za-z0-9]/g, '');
 
+    if(modifiedString === modifiedString.split('').reverse().join('')) {
+        return true;
+    }   else {
+        return false;
+    }
 };
 
 // Do not edit below this line
