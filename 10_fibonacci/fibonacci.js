95b72fa8b98313de96c3081f5f42128d1f4b62c4
mackasfour <amclemon@gmail.com>
2022-12-09 10:17:37 +1100

    fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(raw) {
+    let processedValue = parseInt(raw);
+    let a = 1
+    let b = 0, temp;
 
+    if(processedValue < 0) return "OOPS";
+
+    while (processedValue > 0){
+        temp = a;
+        a = a + b;
+        b = temp;
+        processedValue--;
+    }
+
+    return b;
 };
 
 // Do not edit below this line
