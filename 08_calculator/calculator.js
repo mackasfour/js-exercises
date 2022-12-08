06a4cbf7006da3180b14240f9a02bddfbe1f0533
mackasfour <amclemon@gmail.com>
2022-12-08 18:07:14 +1100

    calculator exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(x,y) {
+  return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x,y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((a, b) => a + b, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((a, b) => a * b);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+
+  let factorialNum = a;
+
+	if(a === 0 || a === 1) {
+    return 1;
+  } 
+
+  while(a > 1) {
+    a--;
+    factorialNum = factorialNum * a;
+  }
+
+  return factorialNum;
 };
 
 // Do not edit below this line
